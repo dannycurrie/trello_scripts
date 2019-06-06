75f843e31a5e31ed25530f9ba90c754d95483abe
Danny Currie <d.currie@elsevier.com>
2019-06-06 10:31:58 +0100

    add script to start sprint

--- setup-sprint.sh
+++ setup-sprint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # REQUIRED ENVIRONMENT VARS:
-# API_KEY
-# TOKEN
+# TRELLO_API_KEY
+# TRELLO_TOKEN
 # WORK_BOARD_ID
 # SPRINT_REVIEW_CARD_TEMPLATE_ID
 # LEARNING_LOG_CARD_TEMPLATE_ID
@@ -11,9 +11,13 @@ BASE_URL="https://api.trello.com/1"
 CARDS_URL="/cards"
 BOARDS_URL="/boards"
 SPRINT_NO="$1"
-AUTH_PARAMS="&key=$API_KEY&token=$TOKEN"
+LIST_NAME="Library%20Sprint%20$SPRINT_NO"
+AUTH_PARAMS="&key=$TRELLO_API_KEY&token=$TRELLO_TOKEN"
 
 # Create new list
+NEW_LIST_URL="$BASE_URL/lists?name=$LIST_NAME&idBoard=$TRELLO_WORK_BOARD_ID&pos=top$AUTH_PARAMS"
+curl --request POST \
+  --url "$NEW_LIST_URL"
 # Add learning log card
 # add sprint review card
 # add learning goals card
